35f560468bd9a025c096eee28dab712cdb844d17
Djonluc <86911215+Djonluc@users.noreply.github.com>
2026-05-08 03:36:08 -0500

    chore: remove unused temporary and development files

--- get_tauri_version.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('src-tauri/Cargo.lock', 'utf8');
-const blocks = content.split('[[package]]');
-for (const block of blocks) {
-    if (block.includes('name = "tauri"') && !block.includes('name = "tauri-')) {
-        console.log(block.trim());
-    }
-}
