324f724875210dfb733eecd19314947401e47931
Djonluc <86911215+Djonluc@users.noreply.github.com>
2026-06-28 18:03:53 -0500

    refactor: remove temporary testing and data extraction scripts

--- fetch_nekos.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-fetch('https://api.nekosapi.com/v4/images/random')
-  .then(res => res.json())
-  .then(data => {
-    fs.writeFileSync('nekos_response.json', JSON.stringify(data, null, 2));
-    console.log("Done");
-  })
-  .catch(err => console.error(err));
